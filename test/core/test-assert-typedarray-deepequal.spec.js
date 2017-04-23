3c84a0ad9ba02da75e3d234e354f1930fbf30621
rmdm <rmdm@users.noreply.github.com>
2017-04-23 12:34:39 +0300

    fix

--- test/core/test-assert-typedarray-deepequal.spec.js
+++ test/core/test-assert-typedarray-deepequal.spec.js
@@ -9,6 +9,27 @@ describe('core assert typedarray deepequal tests', function () {
 
     it('should pass', function () {
 
+        // Copyright Joyent, Inc. and other Node contributors.
+        //
+        // Permission is hereby granted, free of charge, to any person obtaining a
+        // copy of this software and associated documentation files (the
+        // "Software"), to deal in the Software without restriction, including
+        // without limitation the rights to use, copy, modify, merge, publish,
+        // distribute, sublicense, and/or sell copies of the Software, and to permit
+        // persons to whom the Software is furnished to do so, subject to the
+        // following conditions:
+        //
+        // The above copyright notice and this permission notice shall be included
+        // in all copies or substantial portions of the Software.
+        //
+        // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        // OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+        // NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        // USE OR OTHER DEALINGS IN THE SOFTWARE.
+
         function makeBlock(f) {
             var args = Array.prototype.slice.call(arguments, 1);
             return function() {
@@ -28,14 +49,6 @@ describe('core assert typedarray deepequal tests', function () {
             [new Float64Array(1e2), new Float64Array(1e2)],
             [new Int16Array(256), new Uint16Array(256)],
             [new Int16Array([256]), new Uint16Array([256])],
-
-            // a little bit to much for commonjs-assert
-            // [new Float32Array([+0.0]), new Float32Array([-0.0])],
-            // [new Float64Array([+0.0]), new Float32Array([-0.0])],
-            // [new Float64Array([+0.0]), new Float64Array([-0.0])],
-            // [new Uint8Array([1, 2, 3, 4]).subarray(1), new Uint8Array([2, 3, 4])],
-            // [new Uint16Array([1, 2, 3, 4]).subarray(1), new Uint16Array([2, 3, 4])],
-            // [new Uint32Array([1, 2, 3, 4]).subarray(1, 3), new Uint32Array([2, 3])]
         ];
 
         const notEqualArrayPairs = [
@@ -44,12 +57,28 @@ describe('core assert typedarray deepequal tests', function () {
             [new Uint8ClampedArray([300, 2, 3]), new Uint8Array([300, 2, 3])],
             [new Uint16Array([2]), new Uint16Array([3])],
             [new Uint16Array([0]), new Uint16Array([256])],
-            [new Int16Array([0]), new Uint16Array([256])],
-            [new Int16Array([-256]), new Uint16Array([0xff00])], // same bits
-            [new Int32Array([-256]), new Uint32Array([0xffffff00])], // ditto
-            [new Float32Array([0.1]), new Float32Array([0.0])],
-            [new Float64Array([0.1]), new Float64Array([0.0])]
+            [new Int16Array([0]), new Uint16Array([256])]
         ];
+
+        // does not work for node 0.10.x
+        if (process.version >= 'v0.12') {
+
+            equalArrayPairs.push(
+                [new Float32Array([+0.0]), new Float32Array([-0.0])],
+                [new Float64Array([+0.0]), new Float32Array([-0.0])],
+                [new Float64Array([+0.0]), new Float64Array([-0.0])],
+                [new Uint8Array([1, 2, 3, 4]).subarray(1), new Uint8Array([2, 3, 4])],
+                [new Uint16Array([1, 2, 3, 4]).subarray(1), new Uint16Array([2, 3, 4])],
+                [new Uint32Array([1, 2, 3, 4]).subarray(1, 3), new Uint32Array([2, 3])]
+            )
+
+            notEqualArrayPairs.push(
+                [new Int16Array([-256]), new Uint16Array([0xff00])], // same bits
+                [new Int32Array([-256]), new Uint32Array([0xffffff00])], // ditto
+                [new Float32Array([0.1]), new Float32Array([0.0])],
+                [new Float64Array([0.1]), new Float64Array([0.0])]
+            )
+        }
 
         equalArrayPairs.forEach((arrayPair) => {
             // eslint-disable-next-line no-restricted-properties
